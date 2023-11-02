ef0e6d7135494c16a0dd7d7860c9bf3996c9a4b6
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2023-11-02 15:56:15 +0200

    Update ListStatistics.jsx

--- src/components/ListStatistics/ListStatistics.jsx
+++ src/components/ListStatistics/ListStatistics.jsx
@@ -5,11 +5,11 @@ export default function ListStatistics({  good, neutral, bad,total,positivePerce
 
     return (
         <ul className={css.list} >
-            <li className={css.item} key={1}> <ListItem statistic={good} title={'Good'}/> </li>
-            <li className={css.item} key={2}> <ListItem statistic={neutral} title={'Neutral'}/> </li>
-            <li className={css.item} key={3}> <ListItem statistic={bad} title={'Bad'} /> </li>
-            <li className={css.item} key={4}> <ListItem statistic={total} title={'Total'} /> </li>
-            <li className={css.item} key={5}> <ListItem statistic={positivePercentage} title={'Positive feedback'}/> </li>
+            <li className={css.item} > <ListItem statistic={good} title={'Good'}/> </li>
+            <li className={css.item} > <ListItem statistic={neutral} title={'Neutral'}/> </li>
+            <li className={css.item} > <ListItem statistic={bad} title={'Bad'} /> </li>
+            <li className={css.item} > <ListItem statistic={total} title={'Total'} /> </li>
+            <li className={css.item} > <ListItem statistic={positivePercentage} title={'Positive feedback'}/> </li>
         </ul>
     );
 }
